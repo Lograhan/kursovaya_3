0366b55395d2408b38292b26700004486c16d357
roman <romanchrnkv@gmail.com>
2023-05-14 08:18:46 +0300

    Изменен файл main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from utils.class_Operation import Operation
+from utils.func import last_operations
+
+
+data = last_operations()
+
 def main():
-    pass
+    for i in range(len(data)):
+        if i <= len(data):
+            operation = Operation(data[i])
+            print(f'Операция {i + 1}:\n')
+            print(f'{operation.reformat_date()}')
+            if "Перевод со счета на счет" in operation.reformat_date():
+                print(f'{operation.ref_acc_num()} -> {operation.reformat_acc_number()}')
+            else:
+                print(f'{operation.reformat_card_number()} -> {operation.reformat_acc_number()}')
+            print(f'{operation.transfer_amount()}\n')
 
 
 if __name__ == '__main__':
